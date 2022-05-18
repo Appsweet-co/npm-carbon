b44a220ecdea03eaa90ed67dfd7c61d6b5fb8958
dperuo <derek@derekperuo.net>
2022-05-18 17:35:27 -0500

    refactor: (logger.ts) Move everything to one file

--- src/logger.ts
+++ src/logger.ts
@@ -1,43 +1,61 @@
-import { categories, prefix, Types } from "./logger.const";
+import chalk from 'chalk';
 
-export const logger = {
-  docs: (msg: string, emoji: string) => {
-    generic('docs', msg, emoji)
-  },
+type Types = 'docs' | 'error' | 'info' | 'ok' | 'warn';
 
-  error: (msg: string, emoji?: string) => {
-    generic('error', msg, emoji)
-  },
+const categories = {
+  docs: chalk.cyan,
+  error: chalk.red,
+  info: chalk.blue,
+  ok: chalk.green,
+  warn: chalk.yellow,
+};
 
-  info: (msg: string, emoji?: string) => {
-    generic('info', msg, emoji)
-  },
+const prefix = {
+  docs: "==> [DOCS]",
+  error: "==> [ERROR]",
+  info: "==> [INFO]",
+  ok: "==> [OK]",
+  warn: "==> [WARN]",
+};
 
-  ok: (msg: string, emoji?: string) => {
-    generic('ok', msg, emoji)
-  },
-
-  warn: (msg: string, emoji?: string) => {
-    generic('warn', msg, emoji)
-  },
-}
-
-function generic(type: Types, msg: string, emoji?: string) {
+const generic = (type: Types, msg: string, emoji?: string) => {
   const output = (emoji)
     ? `${prefix[type]} ${emoji}  ${msg}`
-    : `${prefix[type]} ${msg}`
+    : `${prefix[type]} ${msg}`;
 
   switch (type) {
     case 'error':
-      console.error(categories.error(output))
+      console.error(categories.error(output));
       break;
 
     case 'warn':
-      console.warn(categories.warn(output))
+      console.warn(categories.warn(output));
       break;
 
     default:
-      console.log(categories[type](output))
+      console.log(categories[type](output));
       break;
   }
-}
+};
+
+export const logger = {
+  docs: (msg: string, emoji: string) => {
+    generic('docs', msg, emoji);
+  },
+
+  error: (msg: string, emoji?: string) => {
+    generic('error', msg, emoji);
+  },
+
+  info: (msg: string, emoji?: string) => {
+    generic('info', msg, emoji);
+  },
+
+  ok: (msg: string, emoji?: string) => {
+    generic('ok', msg, emoji);
+  },
+
+  warn: (msg: string, emoji?: string) => {
+    generic('warn', msg, emoji);
+  },
+};
